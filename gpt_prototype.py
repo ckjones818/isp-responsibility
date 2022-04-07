1d22566ffffecb6a6f61925d212e831133c75376
Colin Jones <ckjones@colgate.edu>
2022-04-07 19:55:59 +0000

    gpt prototype working

---
+++ gpt_prototype.py
@@ -0,0 +1,18 @@
+import openai
+
+openai.api_key = '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'
+
+print('Enter a question:')
+input = input()
+
+response = openai.Completion.create(
+  engine="text-davinci-002",
+  prompt=input,
+  temperature=0,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+
+print(response.choices[0].text)
