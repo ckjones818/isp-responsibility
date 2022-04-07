83e437fa0a059587fd93f0c12bef79f22d5bb308
Omshi Samal <osamal@colgate.edu>
2022-04-07 16:04:34 -0400

    fcc data

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ import json
 import urllib.request
 import os
 
-download_prefix = "https://www.fcc.gov/ecfs/file/download"
+new_prefix = "https://ecfsapi.fcc.gov/filings/file"
 
 api_url = "https://publicapi.fcc.gov/ecfs/filings?api_key=t7IfiKsQgvRs0RSkhGyEKwvo2nHOFVclg9xHY5xa&q=(submissiontype.description:(%22COMPLIANCE%20FILING%22)+AND+proceedings.name:(%2218-142*%22))&limit=100"
 
@@ -15,10 +15,13 @@ url_file = open("document_urls", "w")
 for filing in data:
 	file_url = filing["documents"][0]["src"]
 	doc_id = file_url[file_url.rindex('/'):]
-	doc_url = download_prefix + doc_id
+	doc_url = new_prefix + doc_id
 	filer_id = filing["filers"][0]["name"]
 	filer_id = filer_id.replace(" ", "_").replace(',', '').replace('/', '')
-	url_file.write(doc_url+'\n')
+	
+	new_url = json.loads(requests.get(doc_url).text)["temporary_url"]
+	
+	os.system('curl "{}" --output {}'.format(new_url, filer_id))
 
 
 
